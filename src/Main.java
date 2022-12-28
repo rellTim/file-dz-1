46f234f7db55b209c8fd3636bb6a01baa82e2c1d
Tim <book9902@yandex.ru>
2022-12-28 23:45:29 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,49 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    static void file (){
-        List<String> dirList = Arrays.asList("F:/", "Game","F:/Game", "src","F:/Game", "res","F:/Game", "savegames","F:/Game", "temp","F:/Game/src", "main"
-                ,"F:/Game/src", "test","F:/Game/res", "drawables","F:/Game/res", "vectors","F:/Game/res", "icons");
-        for (int i = 0; i < dirList.size(); i=i+2) {
-            int j = i +1;
-            File dir = new File(dirList.get(i), dirList.get(j));
-            Conclusion.conclusion(dir);
-        }
-        List<String> fileList = Arrays.asList("F:/Game/temp", "temp.txt", "F:/Game/src/main", "Main.java","F:/Game/src/main", "Utils.java");
-        for (int i = 0; i < fileList.size(); i=i+2) {
-            int j = i+1;
-            File file = new File(fileList.get(i), fileList.get(j));
-            Conclusion.conclusionFile(file);
+    static StringBuilder sb = new StringBuilder();
+
+    static void conclusion(File name) {
+        try {
+            if (name.mkdir())
+                sb.append(" Directory " + name.getName() + " - create. ");
+            else
+                System.out.println("not create");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
         }
     }
+
+    static void conclusionFile(File q) {
+        try {
+            if (q.createNewFile())
+                sb.append(" File " + q.getName() + " - create. ");
+            else
+                System.out.println("not create");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void file() {
+        List<String> dirList = Arrays.asList("F:/", "Game", "F:/Game", "src", "F:/Game", "res", "F:/Game", "savegames", "F:/Game", "temp", "F:/Game/src", "main"
+                , "F:/Game/src", "test", "F:/Game/res", "drawables", "F:/Game/res", "vectors", "F:/Game/res", "icons");
+        for (int i = 0; i < dirList.size(); i = i + 2) {
+            int j = i + 1;
+            File dir = new File(dirList.get(i), dirList.get(j));
+            conclusion(dir);
+        }
+        List<String> fileList = Arrays.asList("F:/Game/temp", "temp.txt", "F:/Game/src/main", "Main.java", "F:/Game/src/main", "Utils.java");
+        for (int i = 0; i < fileList.size(); i = i + 2) {
+            int j = i + 1;
+            File file = new File(fileList.get(i), fileList.get(j));
+            conclusionFile(file);
+        }
+    }
+
     public static void main(String[] args) {
         file();
     }
